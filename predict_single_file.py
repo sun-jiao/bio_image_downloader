4b335cecebde44ed1d681e37be2153d44dfdcb67
Sun Jiao <P.ozzy@outlook.com>
2024-08-25 07:41:00 +0800

    minor updates

--- predict_single_file.py
+++ predict_single_file.py
@@ -1,13 +1,19 @@
 # import re
+import json
 
 import torch
 from PIL import Image
 from torchvision.transforms import transforms
 
-from train import get_model
+import metafg_model
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 imgs_path = "data/train/10937.Cryptic Honeyeater/181519057.jpg"
+with open('birdinfo.json', 'r') as f:
+    data = f.read()
+
+# 解码JSON格式的数据
+bird_info = json.loads(data)
 
 
 def image_proprecess(img_path):
@@ -26,7 +32,7 @@ data_transforms = transforms.Compose([
         ])
 
 # 使用模型
-model = get_model('models', 'efv2l_bird', 12000, _freeze=False)
+model = metafg_model.get_metafg_model()
 
 model = model.to(device).eval()
 # model = ipex.optimize(model)
@@ -38,10 +44,12 @@ outputs = model(data)
 
 # 获取最可能的三个类别及其概率
 probs, indices = torch.topk(outputs, k=10, dim=1)
-probs = torch.nn.functional.hardsigmoid(probs)
+probs = torch.nn.functional.softmax(probs, dim=1)
 probs = probs.squeeze().tolist()
 indices = indices.squeeze().tolist()
+indices_zh = [bird_info[indice][0] for indice in indices]
+probs_round = [f'{round(prob * 100, 3)}%' for prob in probs]
 
-print(indices)
-print(probs)
+print(indices_zh)
+print(probs_round)
 
